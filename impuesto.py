fd5f92b19796b79aaa1a3428200e7fd68827fd99
xzpnt <129638449+xzpnt@users.noreply.github.com>
2023-04-08 19:54:47 +0200

    Add files via upload

---
+++ impuesto.py
@@ -0,0 +1,12 @@
+print("<--------------------------->")
+print("<-> Calcule el impuesto   <->")
+print("<--------------------------->")
+precio = float(input("< Precio: "))
+print("<--------------------------->")
+impuesto = float(input("< Impuesto: " ))
+print("<--------------------------->")
+resultado = precio * impuesto
+print("<+> Resultado: "  +  str(resultado))
+print("<--------------------------->")
+
+
